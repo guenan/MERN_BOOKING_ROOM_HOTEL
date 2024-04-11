d62e56ac71a38e8c19b9196ab8cde17752edff5b
lenanguelo <audrey.nangue@googlemail.com>
2024-04-11 07:25:52 +0100

    customize rooms list

--- Rooms.js
+++ Rooms.js
@@ -1,84 +1,84 @@
 var rooms = [
-  {
-    name: "SPOT ON 37669 Hotel Shiva Sai Lodge Near Regimental Bazaar, Shivaji Nagar, Hyderabad",
-    imageurls: [
-      "https://images.oyoroomscdn.com/uploads/hotel_image/56303/medium/597f0e48823f8885.jpg",
-      "https://images.oyoroomscdn.com/uploads/hotel_image/56303/medium/06883e94ec59702e.jpg",
-      "https://images.oyoroomscdn.com/uploads/hotel_image/56303/medium/91dead74a5dffa61.jpg",
-    ],
-    rentperday: 1500,
-  
-    type: "Delux",
-    maxcount: 3,
-    phonenumber:9989649278,
-    currentbookings:[],
-    description:"Hotel Shiva Sai Lodge is a furnished and modest property located in Regimental Bazaar, Shivaji Nagar, Secunderabad, Telangana.The property is in close vicinity to multiple tourist spots namely Sanjeeviah Park, Buddha Statue, Snow World, and Birla Science Museum.",
-  },
-  {
-    name: "OYO Flagship 75243 Metro International",
-    imageurls: [
-      "https://images.oyoroomscdn.com/uploads/hotel_image/105649/large/8e80e24a5496eb80.jpg",
-      "https://images.oyoroomscdn.com/uploads/hotel_image/105649/large/caef038a4b97b589.jpg",
-      "https://images.oyoroomscdn.com/uploads/hotel_image/105649/large/46bffe91d06dcb96.jpg",
-    ],
-    rentperday: 1500,
-   
-    type: "Delux",
-    maxcount: 3,
-    phonenumber:9989649278,
-    currentbookings:[],
-    description:"Hotel Shiva Sai Lodge is a furnished and modest property located in Regimental Bazaar, Shivaji Nagar, Secunderabad, Telangana.The property is in close vicinity to multiple tourist spots namely Sanjeeviah Park, Buddha Statue, Snow World, and Birla Science Museum.",
-  },
-  {
-    name: "OYO FLAGSHIP 76687 Ramoji Kothapet",
-    imageurls: [
-      "https://images.oyoroomscdn.com/uploads/hotel_image/107625/large/4488071a59d38cc5.jpg",
-      "https://images.oyoroomscdn.com/uploads/hotel_image/107625/large/d452b4897a767f6b.jpg",
-      "https://images.oyoroomscdn.com/uploads/hotel_image/107625/large/8f690e54761344e0.jpg",
-    ],
-    rentperday: 2000,
-   
-    type: "Non-Delux",
-    maxcount: 3,
-    phonenumber:9989649278,
-    currentbookings:[],
-    description:"Hotel Shiva Sai Lodge is a furnished and modest property located in Regimental Bazaar, Shivaji Nagar, Secunderabad, Telangana.The property is in close vicinity to multiple tourist spots namely Sanjeeviah Park, Buddha Statue, Snow World, and Birla Science Museum.",
-  },
-  {
-    "name":"OYO 80211 Collection O Sri Navya Grand",
-    "imageurls":[
-       "https://images.oyoroomscdn.com/uploads/hotel_image/111796/large/e225bbf160a11c02.jpg",
-       "https://images.oyoroomscdn.com/uploads/hotel_image/111796/large/d55361043c9b52af.jpg",
-       "https://images.oyoroomscdn.com/uploads/hotel_image/111796/large/2f7029d3c5429b5f.jpg"
-    ],
-    "rentperday":1600,
-    
-    "type":"Non-Delux",
-    "maxcount":4,
-    "phonenumber":9989649278,
-    "currentbookings":[
-       
-    ],
-    "description":"Couples are welcome Guests can check in using any local or outstation ID proof (PAN card not accepted). Only Indian Nationals allowed As a complimentary benefit, your stay is now insured by Acko. This hotel is serviced under the trade name of Sri Navya Grand as per quality standards of OYO"
-} ,
+    {
+        name: "Panoramic Suite",
+        imageurls: [
+            "https://images.oyoroomscdn.com/uploads/hotel_image/56303/medium/597f0e48823f8885.jpg",
+            "https://images.oyoroomscdn.com/uploads/hotel_image/56303/medium/06883e94ec59702e.jpg",
+            "https://images.oyoroomscdn.com/uploads/hotel_image/56303/medium/91dead74a5dffa61.jpg",
+        ],
+        rentperday: 1500,
 
-{
-    "name":"Capital O 15959 The City Park",
-    "imageurls":[
-       "https://images.oyoroomscdn.com/uploads/hotel_image/38668/large/414e401312ad45a7.jpg",
-       "https://images.oyoroomscdn.com/uploads/hotel_image/38668/large/d24b920cfdddeecb.jpg",
-       "https://images.oyoroomscdn.com/uploads/hotel_image/38668/large/58025558878fa4ec.jpg"
-    ],
-    "rentperday":1800,
-    
-    "type":"Non-Delux",
-    "maxcount":3,
-    "phonenumber":9989649278,
-    "currentbookings":[
-       
-    ],
-    "description":"Night Curfew in Telangana | Check-in allowed between 05:00 AM to 09:00 PM Couples are welcome Guests can check in using any local or outstation ID proof (PAN card not accepted). As a complimentary benefit, your stay is now insured by Acko.This hotel is serviced under the trade name of The City Park as per quality standards of OYO"
-}
+        type: "Delux",
+        maxcount: 3,
+        phonenumber:9989649278,
+        currentbookings:[],
+        description:"Indulge in breathtaking city views from our panoramic suite. With floor-to-ceiling windows, this suite offers an unparalleled visual experience, perfect for lovers of spectacular views.",
+    },
+    {
+        name: "Deluxe Room International",
+        imageurls: [
+            "https://images.oyoroomscdn.com/uploads/hotel_image/105649/large/8e80e24a5496eb80.jpg",
+            "https://images.oyoroomscdn.com/uploads/hotel_image/105649/large/caef038a4b97b589.jpg",
+            "https://images.oyoroomscdn.com/uploads/hotel_image/105649/large/46bffe91d06dcb96.jpg",
+        ],
+        rentperday: 1500,
+
+        type: "Delux",
+        maxcount: 3,
+        phonenumber:9989649278,
+        currentbookings:[],
+        description:"Unwind in the comfort of our deluxe room. Spacious and elegantly appointed, this room is perfect for those seeking a high-end stay experience.",
+    },
+    {
+        name: "Executive Suite",
+        imageurls: [
+            "https://images.oyoroomscdn.com/uploads/hotel_image/107625/large/4488071a59d38cc5.jpg",
+            "https://images.oyoroomscdn.com/uploads/hotel_image/107625/large/d452b4897a767f6b.jpg",
+            "https://images.oyoroomscdn.com/uploads/hotel_image/107625/large/8f690e54761344e0.jpg",
+        ],
+        rentperday: 2000,
+
+        type: "Non-Delux",
+        maxcount: 3,
+        phonenumber:9989649278,
+        currentbookings:[],
+        description:"Experience luxury and sophistication in our executive suite. Designed to meet all your professional and personal needs, this suite offers a stylish space to work and relax.",
+    },
+    {
+        "name":"Sea View Room",
+        "imageurls":[
+            "https://images.oyoroomscdn.com/uploads/hotel_image/111796/large/e225bbf160a11c02.jpg",
+            "https://images.oyoroomscdn.com/uploads/hotel_image/111796/large/d55361043c9b52af.jpg",
+            "https://images.oyoroomscdn.com/uploads/hotel_image/111796/large/2f7029d3c5429b5f.jpg"
+        ],
+        "rentperday":1600,
+
+        "type":"Non-Delux",
+        "maxcount":4,
+        "phonenumber":9989649278,
+        "currentbookings":[
+
+        ],
+        "description":"Let yourself be lulled by the soothing sound of the waves in our sea view room. Enjoy tranquility and natural beauty as you relax in this coastal retreat."
+    } ,
+
+    {
+        "name":"Royal Suite",
+        "imageurls":[
+            "https://images.oyoroomscdn.com/uploads/hotel_image/38668/large/414e401312ad45a7.jpg",
+            "https://images.oyoroomscdn.com/uploads/hotel_image/38668/large/d24b920cfdddeecb.jpg",
+            "https://images.oyoroomscdn.com/uploads/hotel_image/38668/large/58025558878fa4ec.jpg"
+        ],
+        "rentperday":1800,
+
+        "type":"Non-Delux",
+        "maxcount":3,
+        "phonenumber":9989649278,
+        "currentbookings":[
+
+        ],
+        "description":"Live like royalty in our royal suite. With luxurious amenities and sumptuous ambiance, this suite offers an incomparable experience for those seeking the ultimate in comfort."
+    }
 ];
 
 // export default rooms
